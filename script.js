801fe3b41957c93eb3b978c290c570f0375fe5ac
obiorachidera <obiorachidera596@gmail.com>
2023-08-16 00:25:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function addTenNumber (number1,number2,number3,number4,number5,number6,number7,number8,number9,number10){
+    return number1+number2+number3+number4+number5+number6+number7+number8+number9+number10;
+}
+let arrays = addTenNumber(1,2,3,4,5,6,7,8,9,10)
+console.log(arrays); 
+for (let i= 1; i=addTenNumber.add; i++) {
+     console.log(arrays)
+};
+  
+     
